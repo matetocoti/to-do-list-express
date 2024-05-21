0af83b98f607450ccbb376d8db9c61e1216a05ad
Matheus Costa <matheuscosta_111@hotmail.com>
2024-05-21 16:41:12 -0300

    aula-01

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+//Lib express  
+//para  o desenvolvimento de aplicações web e APIs.
 const express = require("express")
+
+
+//Deixando disnponivel os métodos da lib express na constante app
+const app = express()
+
+
+//Criando rota 
+//("/"") --> chamada do root
+//(requisição ,resposta)
+app.get("/" ,(requisition ,response) => {
+    response.send("<h1>Minha lista de tarefas</h1>")
+})
+
+
+//Definindo porta do servidor
+app.listen(3000, () => {
+    console.log("O servidor foi iniciado")
+})
