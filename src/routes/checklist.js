9d64c6ccbb4b2a523bc9ee67cbcd9152a53aa4dd
Matheus Costa <matheuscosta_111@hotmail.com>
2024-05-22 02:27:19 -0300

    Parametros nas rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -9,11 +9,30 @@ router.get("/" ,(requisition ,response) => {
     response.send() 
 })
 
-//Nesse caso mesmo com mesmo caminho se o tipo for post ele serar mandado para esse endpoint
+
+//Exemplo no java
+
+//@GetMapping(value = "/{id}")
+//public ResponseEntity<User> findById(@PathVariable Long id)
+
+//Eu passo um parametro para ser respondido pela url 
+//Exemplo: http://localhost:8765/hr-user/users/1
+//Nesse caso o parametro id faz parte da url
+router.get("/:id" ,(requisition ,response) => {
+    console.log(requisition.params.id) //Exibindo parametro da função no console
+    response.send(`ID: ${requisition.params.id}`) //Devolvendo o parametro id como resposta da requisição 
+})
+
+
+
+
+
+
+
 router.post("/" ,(requisition ,response) => {
-    console.log(requisition.body) //Pegando o body da requisição
-    response.status(200) //Definindo o status da requisição como 200
-    response.send(requisition.body) //Retornando o body
+    console.log(requisition.body) 
+    response.status(200) 
+    response.send(requisition.body) 
 })
 
 
@@ -30,5 +49,6 @@ router.post("/" ,(requisition ,response) => {
 
 
 
-//Exportando o modulo|router
+
+
 module.exports = router
