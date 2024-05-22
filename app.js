f2cc3183c142951b1a8fbc05d834eb6b3fee0db0
Matheus Costa <matheuscosta_111@hotmail.com>
2024-05-22 02:04:00 -0300

    Rota post

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ const checkListRouter = require("./src/routes/checklist")
 
 const app = express()
 app.use(express.json())
- //Uso o obj de rotas router criado no arquivo checklist como um middleware.js
-
-//Como eu acho que 'seria' em Java
-//Coloquei esse exemplo pois é a linguagem que estou mais acustumado
-//@RestController
-//@RequestMapping(value = "/checklists")
- app.use("/checklists" ,checkListRouter) //Todas as rotas sao derivadas da rota /checklists 
+app.use("/checklists" ,checkListRouter) 
 
 
 
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,15 +1,31 @@
 const express = require("express")
 
 
-//Permiti criar rotas em varios arquivos e depois chamar no arquivo principal
 const router = express.Router()
 
 
-//Criando rota apartir do router
 router.get("/" ,(requisition ,response) => {
     console.log("Hello, world!")
     response.send() 
 })
+
+//Nesse caso mesmo com mesmo caminho se o tipo for post ele serar mandado para esse endpoint
+router.post("/" ,(requisition ,response) => {
+    console.log(requisition.body) //Pegando o body da requisição
+    response.status(200) //Definindo o status da requisição como 200
+    response.send(requisition.body) //Retornando o body
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
